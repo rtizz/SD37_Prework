12e8105f16091637ece72ff1ccf641c6ae5e4855
rob.tisdale <rtisdale.bt@gmail.com>
2022-12-06 20:27:30 -0600

    Obj2 Complete

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Robert Tisdale";
+    int age = 32;
+    String state = "Florida";
+
+   
+   System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+int yearInt = 1990;
+String monthString = "November";
+boolean isWeekend = false;
+
+System.out.println(yearInt + "\n" + monthString + "\n" + isWeekend);
+
+  }
+}
