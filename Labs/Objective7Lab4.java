28ab5fd95bf776bdb707aaf73266d09fadd5ba4d
rob.tisdale <rtisdale.bt@gmail.com>
2022-12-08 21:57:45 -0600

    Objective 6 & 7 Complete

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+
+		int count = 1;
+		int sum = 0;
+
+		while (count < 21) {
+			sum = sum + count;
+			count = count + 1;
+		}
+
+		System.out.println(sum);
+
+	}
+}
